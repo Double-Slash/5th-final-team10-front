3fbff67e0bd7947bab2d71e1e2343b342807e27f
kimsutto <themerciless2016@gmail.com>
2021-01-17 02:21:16 +0900

    Style : GlobalStyle 설정, 디자인 패턴

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.css';
 import App from './App';
+import GlobalStyle from './styles/global-style';
+import { ThemeProvider } from 'styled-components';
+import {theme} from './styles/theme';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme = {theme}>
+    <GlobalStyle/>
+      <App />
+  </ThemeProvider>,
   document.getElementById('root')
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,21 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme = {
+    main_color : "#418AFF",
+    hover_color : "#2D59B8",
+
+    while : "#FFFFFF",
+    grey : "#C4C4C4",
+    light_grey : "#EFEFEF",
+    text_grey : "#898989",
+    light_blue : "#D7E6FF",
+    black : "#000000",
+
+    footer_bg_color : "#F6F6F8",
+    footer_font_color : "#6C6C6C",
+}
+
+/*
+사용 시 
+${props => props.theme.main_color}; 
+*/
